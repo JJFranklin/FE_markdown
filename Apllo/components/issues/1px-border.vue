a8dfd5463da9b112fbeb19e947eac95ed09928bc
JJFranklin <13437225395@163.com>
2021-01-18 23:38:57 +0800

    添加技术问题

---
+++ Apllo/components/issues/1px-border.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class='border'>
+问题
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+
+    },
+    methods: {
+
+    },
+    data() {
+        return {
+
+        };
+    },
+    computed: {
+
+    },
+    watch: {
+
+    },
+    mounted() {
+
+    },
+};
+</script>
+
+<style scoped lang='scss'>
+
+</style>
