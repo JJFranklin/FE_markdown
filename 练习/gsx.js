19a8cddc48581ca90ee9b762df3f0159220e31fc
JJFranklin <JJFranklin@https://github.com>
2021-04-02 17:09:26 +0800

    添加练习内容

--- 练习/gsx.js
+++ 练习/gsx.js
@@ -210,9 +210,15 @@ async function timeout(){
 timeout();
 
 
+function* gen(x) {
+    var y = yield x + 2;
+    console.log();
+    return y;
+}
 
-
-
+var g = gen(1);
+g.next() // { value: 3, done: false }
+g.next() 
 
 
 
