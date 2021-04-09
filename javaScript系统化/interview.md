140abcd104fd94ef1eaebcb7b661d6284c2d1738
JJFranklin <JJFranklin@https://github.com>
2021-04-09 16:01:50 +0800

    完成promise 的实现

--- javaScript系统化/interview.md
+++ javaScript系统化/interview.md
@@ -7,3 +7,6 @@ https://muyiy.cn/question/
 https://blog.windstone.cc/vue/source-study/observer/
 
 https://github.com/mqyqingfeng/Blog
+
+vue 基本知识点
+http://interview.poetries.top/principle-docs/vue/10-%E5%9B%BE%E8%A7%A3%20Vue%20%E5%93%8D%E5%BA%94%E5%BC%8F%E5%8E%9F%E7%90%86.html#%E6%A8%A1%E6%9D%BF%E6%B8%B2%E6%9F%93
--- 练习/newPromise.js
+++ 练习/newPromise.js
@@ -15,6 +15,12 @@ promise.then((res) => {
 
 });
 
+promise.then((res) => {
+
+}).catch((err) => {
+
+});
+
 function isFunction(func) {
     return typeof func === "function";
 }
@@ -29,35 +35,94 @@ class NewPromise {
             throw new Error("参数必须是函数！");
         }
         this.status = PENDING; // 状态初始化为PENDING 
-        this.value = null; // 初始化结果
+        this.value = null; // 初始化结果，回调函数返回的值
+        this.fulfilledArr = [];
+        this.rejectedArr = []; // 存储等待执行回调的队列
 
         handler(this.newResolved.bind(this), this.newRejected.bind(this));
     }
     newResolved(val) {
-        if (this.status === PENDING) return;
-        // resolve传入的值是是一个新的promise实例
-        // 需要不断回调得到最后非 peomise 对象的值
-        if (val instanceof NewPromise) {
-            val.newThen(res => {
-                this.value = res;
-            });
-        } else {
-            // 普通值类型的值
-            this.value = val;
-        }
+        if (this.status !== PENDING) return;
+        this.value = val;
         this.status = FULFILLED;
+        this.fulfilledArr.map(cb => cb(val));
     }
 
-    newRejected() {
-
+    newRejected(err) {
+        if (this.status !== PENDING) return;
+        this.value = err;
+        this.status = REJECTED;   this.rejectedArr.map(cb => cb(val));
     }
+     
+    // then 返回一个新的promise对象
     newThen(onFulilled, onRejected) {
+        let {
+            status,
+            value
+        } = this;
 
+        return new NewPromise((resolvedThen, rejectedThen) => {
+            // 成功时执行的函数
+            let fulilledAction = (result) => {
+                // 如果then的第一个不是函数，是基本类型的值，直接返回数据
+                if (!isFunction(onFulilled)) {
+                    resolvedThen(result);
+                } else {
+                    // 是函数类型
 
+                    // 拿到成功回调的结果
+                    // 如果成功回调的结果是 promise对象，
+                    // 需要等待上一个promise对象执行完毕，才能执行新的promise 对象的回调，拿到最新的结果
+                    // 成功回调如果不是 promise 对象，直接resolve，返回最后的结果
+                    let res = onFulilled(result);
+                    if (res instanceof NewPromise) {
+                        res.newThen(resolvedThen, rejectedThen);
+                    } else {
+                        resolvedThen(res);
+                    }
+                }
+            }
+
+            let rejectedAction = (result) => {
+                if (!isFunction(onRejected)) {
+                    rejectedThen(result);
+                } else {
+                    // 是函数类型
+                    let res = onRejected(result);
+                    if (res instanceof NewPromise) {
+                        res.newThen(resolvedThen, rejectedThen);
+                    } else {
+                        rejectedThen(res);
+                    }
+                }
+
+            }
+
+            // 返回成功的状态，立即执行成功回调；
+            // 返回失败状态，立即执行失败回调
+            // 不是这两种状态将当前状态存储起来，等待执行，到then的时候，
+            // 状态不一定变成了成功或者失败，需要将回调暂存起来，等到变成了成功或者失败，再执行
+
+            switch (status) {
+                case PENDING:
+                    this.fulfilledArr.push(onFulilled);
+                    this.rejectedArr.push(onRejected);
+                    break;
+                case FULFILLED:
+                    fulilledAction(value);
+                    break;
+                case REJECTED:
+                    rejectedAction(value);
+                    break;
+                default:
+            }
+        });
     }
 }
-new NewPromise(function (resolved, rejected) {
-    resolved("1234")
+let p = new NewPromise(function (resolved, rejected) {
+    resolved("1234666")
 });
-// // let p  = 
-// p.newResolved();
+
+p.newThen(res => {
+    console.log("res", res);
+});
