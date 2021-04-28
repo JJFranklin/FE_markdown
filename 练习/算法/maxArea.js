d3c41d392926b4dadbb9a6a371e0869750d0a987
JJFranklin <13437225395@163.com>
2021-04-28 23:03:58 +0800

    添加算法

---
+++ 练习/算法/maxArea.js
@@ -0,0 +1,14 @@
+// 盛水最多的容器
+// 动态规划法，思路：什么情况面积最大
+var maxArea = function (arr) {
+    if (!Array.isArray(arr) || arr.length <= 1) return;
+    let maxArea = 0;
+    for (let i = 0, j = arr.length - 1; i < j;) {
+        let minVal = arr[i] < arr[j] ? arr[i++] : arr[j--];
+        maxArea = Math.max(maxArea, minVal * (j - i + 1));
+    }
+    return maxArea;
+};
+
+let arr = [1,8,6,2,5,4,8,3,7];
+console.log("getMaxArea", maxArea(arr));
