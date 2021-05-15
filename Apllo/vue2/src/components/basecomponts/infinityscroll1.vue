87f0f6981c2e2f797a10e015b31ecd2fd16c2d06
JJFranklin <13437225395@163.com>
2021-05-16 01:59:58 +0800

    完成无线加载1

--- Apllo/vue2/src/components/basecomponts/infinityscroll1.vue
+++ Apllo/vue2/src/components/basecomponts/infinityscroll1.vue
@@ -1,20 +1,24 @@
 
 <template>
-  <div class="good-container">
-    <!-- <el-button type="primary" @click.stop="requiredata">再次请求数据</el-button> -->
-    <div class="row-header">
-      <span class="header-title">序号</span>
-      <span class="header-title">商品名称</span>
-      <span class="header-title">价格</span>
-      <span class="header-title">描述</span>
+    <div class="good">
+        <div class="row-header">
+            <span class="header-title">序号</span>
+            <span class="header-title">商品名称</span>
+            <span class="header-title">价格</span>
+            <span class="header-title">描述</span>
+        </div>
+        <div class="goodlist" @scroll.passive="debounceScroll">
+            <div class="row" v-for="item in list" :key="item.index">
+                <span class="good-item no">{{ item.index }}</span>
+                <span class="good-item name">{{ item.name }}</span>
+                <span class="good-item price">{{ item.price }}</span>
+                <span class="good-item des">{{ item.des }}</span>
+            </div>
+            <div class="loading-container" v-if="isLoading">
+                <span class="el-icon-loading"></span>加载中...
+            </div>
+        </div>
     </div>
-    <div class="row" v-for="item in goodList" :key="item.index">
-      <span class="good-item no">{{ item.index }}</span>
-      <span class="good-item name">{{ item.name }}</span>
-      <span class="good-item price">{{ item.price }}</span>
-      <span class="good-item des">{{ item.des }}</span>
-    </div>
-  </div>
 </template>
 
 <script>
@@ -26,58 +30,118 @@
  * 4、防抖处理，掌握触发时机
  * 5、返回的参数和原来的参数拼接在一起
  * 6、先实现，在留接口
+ * ele.scrollHeight 滚动区域的高度，也就实际内容的高度
+ * ele.scrollTop  滚动条距离顶部的高度
+ * ele.clientHeight 可视区域的高度
+ *
+ * 滚动条距离底部的距离 ele.scrollHeight - ele.scrollTop - ele.clientHeight
+ *
+ * 滚动条距离底部一定的距离重新加载数据，思路
+ * 1、计算距离滚动条距离底部的距离 实际内容高度（scrollHeight） - 滚动条距离顶部的高度（scrollTop） - 可视内容高度（clientHeight）
+ * 2、记录上一次请求是否完成；
+ * 3、混动条的滚动方向：通过判断上一次滚动条的位置和现在滚动条的位置
+ * 4、记录数据是否加载完毕，避免加载完了，还发送请求
+ * 5、每次load完，或者所有数据加载完了，loading 效果去掉
  */
 //
 import { mapGetters, mapActions, mapState } from "vuex";
-export default {
-  props: {},
-  data() {
-    return {};
 
-  },
-  methods: {
-    ...mapActions(["getAllGoodList", "getGoodListByParam"]),
-    // requiredata(){
-    //   // 模拟分页请求；
-    //   let param  = Object.assign({},{page:this.pageInfo.page + 1});
-    //   this.getGoodListByParam(param);
-    // }, 
-  },
-  computed: {
-    ...mapGetters(["goodList", "pageInfo"]),
-  },
-  watch: {},
-  mounted() {
-    this.getAllGoodList();   
-  },
+export default {
+    props: {},
+    data() {
+        return {
+            list: [],
+            eleScrollBottom: "",
+            scrollTop: 0, // 初始滚动条距离顶部的距离
+            triggerBottomHeight: 10,
+            loaded: false, // 记录数据是否加载完毕
+            isScrollDown: true, // 滚动条滚动方向 up 向上滚动 默认向下滚动 down
+            isLoading: false, // 是否出现加载动画
+            wait: 500, // 等待时间
+        };
+    },
+    methods: {
+        ...mapActions(["getAllGoodList", "getGoodListByParam"]),
+        calculateScrollBottom(e) {
+            let ele = e.target;
+            let eleScrollHeight = ele.scrollHeight;
+            let eleClientHight = ele.clientHeight;
+            this.isScrollDown = ele.scrollTop - this.scrollTop > 0;
+            this.scrollTop = ele.scrollTop;
+            this.eleScrollBottom = eleScrollHeight - eleClientHight - this.scrollTop;
+        },
+
+        debounceScroll(e) {
+            this.loaded = false;
+            this.isLoading = this.goodList.length == this.pageInfo.size;
+            this.calculateScrollBottom(e);
+            if (
+                this.eleScrollBottom < this.triggerBottomHeight &&
+                // !this.loaded &&
+                this.isScrollDown &&
+                this.goodList.length == this.pageInfo.size
+            ) {
+                _.debounce(this.loadMore, this.wait)();
+            }
+        },
+        loadMore() {
+            // 模拟分页请求；
+            let param = Object.assign({}, { page: this.pageInfo.page + 1 });
+            console.log("加载更多...");
+            this.getGoodListByParam(param);
+            this.list = [...this.list, ...this.goodList];
+            this.loaded = true;
+            this.isLoading = false;
+        },
+    },
+    computed: {
+        ...mapState({
+            goodList: (state) => state.good.goodList,
+            pageInfo: (state) => state.good.pageInfo,
+        }),
+    },
+    watch: {},
+    created() {
+        this.list = [];
+        this.getAllGoodList().then((res) => {
+            this.list = [...this.list, ...this.goodList];
+        });
+    },
 };
 </script>
 
 <style scoped lang='scss'>
-.good-container {
-  max-height: 800px;
-  overflow-y: auto;
-  overflow-x: hidden;
-  .row {
-    &-header {
-      padding: 15px 0;
-      background: #f5f7fa;
+.good {
+    .goodlist {
+        height: 800px;
+        overflow-y: auto;
+        overflow-x: hidden;
+        .row {
+            &-header {
+                padding: 15px 0;
+                background: #f5f7fa;
+            }
+            padding: 15px 24px;
+            box-sizing: border-box;
+            border-bottom: 1px solid #eee;
+        }
     }
-    padding: 15px 24px;
-    box-sizing: border-box;
-    border-bottom: 1px solid #eee;
-  }
-  .good-item,
-  .header-title {
-    display: inline-block;
-    min-width: 120px;
-    line-height: 32px;
-    height: 32px;
-    padding-left: 15px;
-    box-sizing: border-box;
-    &.no {
-      text-align: left;
+    .good-item,
+    .header-title {
+        display: inline-block;
+        min-width: 120px;
+        line-height: 32px;
+        height: 32px;
+        padding-left: 15px;
+        box-sizing: border-box;
+        &.no {
+            text-align: left;
+        }
     }
-  }
+    .loading-container {
+        line-height: 32px;
+        height: 32px;
+        text-align: center;
+    }
 }
 </style>
--- Apllo/vue2/store/modules/good.js
+++ Apllo/vue2/store/modules/good.js
@@ -1,66 +1,80 @@
 import axios from "axios";
 
 export default {
-    state:{
-        goodAllList:[], // 所有数据
-        goodList:[], // 分页数据
-        pageInfo:{
-            page:0,// 当前页数据
-            size:20,// 每页的数据
-        }
+    state: {
+        goodAllList: [], // 所有数据
+        goodList: [], // 分页数据
+        pageInfo: {
+            page: 0, // 当前页数据
+            size: 20, // 每页的数据
+        },
     },
-    getters:{
-        goodAllList(state,getters,rootstate){
+    getters: {
+        goodAllList(state, getters, rootstate) {
             return state.goodAllList;
             // 返回1000条数据
         },
-        goodList(state){
+        goodList(state) {
             return state.goodList;
         },
-        pageInfo(state){
+        pageInfo(state) {
             return state.pageInfo;
         }
     },
-    mutations:{
-        getGoodItem(state,result){
+    mutations: {
+        getGoodItem(state, result) {
             state.goodAllList = [];
             state.goodList = [];
-            let {name,price,des} = result;
-            for (let i = 0; i < 999; i++) {
+            let {
+                name,
+                price,
+                des
+            } = result;
+            for (let i = 0; i < 23; i++) {
                 state.goodAllList.push({
-                    index:i+1,
-                    name:`${name}${i + 1}`,
-                    price:`${price + i}`,
-                    des:`第${i+1}${des}`
+                    index: i + 1,
+                    name: `${name}${i + 1}`,
+                    price: `${price + i}`,
+                    des: `第${i + 1}${des}`
                 })
             }
         },
-        getGoodListByParam(state,params){
-            let {page} = params;
-            let {size} = state.pageInfo;
-            console.log({...params});
+        getGoodListByParam(state, params) {
+            let {
+                page
+            } = params;
+            let {
+                size
+            } = state.pageInfo;
+
             let startPage = page * size;
-            let endPage= (page + 1) * size;
+            let endPage = (page + 1) * size;
 
             state.pageInfo.page = page;
-
-            let curGoodList = state.goodAllList.slice(startPage,endPage);
-            
-            state.goodList = [...state.goodList,...curGoodList];
+            state.goodList = state.goodAllList.slice(startPage, endPage);
         }
     },
-    actions:{
-        getAllGoodList({commit,dispatch,state}){
-            axios.get("/api/good.json").then(res=>{
-                if(200 == res.status){
-                    commit("getGoodItem",res.data)
+    actions: {
+        getAllGoodList({
+            commit,
+            dispatch,
+            state
+        }) {
+            return axios.get("/api/good.json").then(res => {
+                if (200 == res.status) {
+                    commit("getGoodItem", res.data)
                 }
-            }).then(()=>{
-                dispatch("getGoodListByParam",{page:0,size:state.pageInfo.size});
+            }).then(() => {
+                commit("getGoodListByParam", {
+                    page: 0,
+                    size: state.pageInfo.size
+                });
             });
         },
-        getGoodListByParam({commit},params){
-            commit("getGoodListByParam",params);
+        getGoodListByParam({
+            commit
+        }, params) {
+            commit("getGoodListByParam", params);
         }
     }
 }
