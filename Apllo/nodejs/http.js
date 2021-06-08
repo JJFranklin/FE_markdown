dbd42aecbbacb26b8199416bab45abb4e63ae212
JJFranklin <13437225395@163.com>
2021-06-08 23:28:46 +0800

    整理webpack遗漏点

--- Apllo/nodejs/http.js
+++ Apllo/nodejs/http.js
@@ -14,12 +14,6 @@ const server = http.createServer(function (req, res) {
             })
         }).then((result)=>{
             res.end(JSON.stringify(result));
-        }).catch((err)=>{
-
-        });
+        }).catch((err)=>{});
     }
-    // res.setHeader("Access-Control-Allow-Headers", "X-Requested-With");
-    // res.setHeader("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-    // res.setHeader("X-Powered-By",' 3.2.1');
- 
 }).listen(8070, "127.0.0.1");
