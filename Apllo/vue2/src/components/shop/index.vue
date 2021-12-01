050d6bfe5eb7b11d02862193fc7ad6bef2df0724
JJFranklin <JJFranklin@github.com>
2021-12-02 00:48:51 +0800

    添加新的格式化代码

--- Apllo/vue2/src/components/shop/index.vue
+++ Apllo/vue2/src/components/shop/index.vue
@@ -1,42 +1,28 @@
 <template>
-    <div class=''>
-        <shopproduct></shopproduct>
-        <shopcart></shopcart>
-
-    </div>
+  <div class="">
+    <shopproduct></shopproduct>
+    <shopcart></shopcart>
+  </div>
 </template>
 
 <script>
-import shopcart from '@components/shop/shopcart';
-import shopproduct from '@components/shop/shopproduct';
+import shopcart from "@components/shop/shopcart";
+import shopproduct from "@components/shop/shopproduct";
 export default {
-    props: {
-
-    },
-    data() {
-        return {
-
-        };
-    },
-    methods: {
-
-    },
-    computed: {
-
-    },
-    watch: {
-
-    },
-    mounted() {
-
-    },
-    components:{
-        shopcart,
-        shopproduct
-    }
+  props: {},
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {},
+  watch: {},
+  mounted() {},
+  components: {
+    shopcart,
+    shopproduct,
+  },
 };
 </script>
 
 <style scoped lang='scss'>
-
 </style>
