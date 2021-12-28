ba49a3627db5cc8e89047ef89e30a4632b7093e6
JJFranklin <13437225395@163.com>
2021-12-28 17:59:09 +0800

    添加书籍目录

--- javaScript系统化/js 拾遗.js
+++ javaScript系统化/js 拾遗.js
@@ -1,8 +1,9 @@
 /**
  * 1、函数参数设置默认值，只有转入参数是 undefined 的时候，才会使用默认值，
  * 其他的假值情况，如，false,0,'',null,使用的是传入的值；
- * 2、使用 Object.assign({key:defaultValue},Obj); 设置默认值
- * 
+ * 2、使用 Object.assign({key:defaultValue},Obj) 设置默认值
+ * 3、不用添加区全局的函数，在类里面添加属性，属性值是函数
+ * 4、使用函数式编程，不使用命令式编程，面向过程编程
  * 
 */
 
