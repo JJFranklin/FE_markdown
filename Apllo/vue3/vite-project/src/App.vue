c2829ab22fcec258e91165aee4134069c59f1431
hemingming <hemingming@megvii.com>
2024-01-24 21:26:39 +0800

    vite + vue3 项目中添加tailwindcss

--- Apllo/vue3/vite-project/src/App.vue
+++ Apllo/vue3/vite-project/src/App.vue
@@ -3,13 +3,8 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="bg-red-600">
+    我是一个兵
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
